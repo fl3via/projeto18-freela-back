aa7615703b1eac38b271c2f29b8d02fb07785b44
Flavia Mendes <flaviamendesdasilva99@gmail.com>
2023-08-08 10:55:47 -0300

    Commit inicial: adicioando estrutura do projeto

---
+++ src/controllers/app.controllers.js
@@ -0,0 +1,29 @@
+export async function cadastro(req, res) {
+    try {
+
+    } catch (err) {
+        res.status(500).send(err.message)
+      
+
+    }
+}
+
+export async function login(req, res) {
+    try {
+
+    } catch (err) {
+        res.status(500).send(err.message)
+      
+
+    }
+}
+
+export async function statusDoProduto(req, res) {
+    try {
+
+    } catch (err) {
+        res.status(500).send(err.message)
+      
+
+    }
+}
---
+++ src/routes/index.routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+
+
+const router = Router()
+
+router.use()
+router.use()
+router.use()
+router.use()
+
+export default router
